044103f94c86f398245c3fa348cea0bd4975be79
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-04-19 14:44:28 -0400

    Started binary_expression_tree.py

---
+++ M5 Trees and Priority Queues Project/binary_expression_tree.py
@@ -0,0 +1,9 @@
+from stack import Stack
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryExpressionTree:
