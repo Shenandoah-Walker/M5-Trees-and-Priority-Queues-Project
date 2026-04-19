c1243b943cc0c577a8878eca4dc0161ea9bcb37f
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-04-19 15:21:02 -0400

    continued working on binary_expression_tree.py

--- M5 Trees and Priority Queues Project/binary_expression_tree.py
+++ M5 Trees and Priority Queues Project/binary_expression_tree.py
@@ -7,5 +7,32 @@ class TreeNode:
         self.right = None
 
 class BinaryExpressionTree:
-    def __init__(self, expression):
-        self.root = TreeNode(expression)
+    def __init__(self):
+        self.root = None
+
+    def is_empty(self):
+        if self.root == None:
+            return True
+        else:
+            return False
+
+    def clear_tree(self):
+        self.root = None
+
+    def build_from_postfix(self, postfix_expression):
+
+        stack = Stack()
+        for token in postfix_expression:
+            if token.isdigit():
+                node = TreeNode(token)
+                stack.push(node)
+
+            elif token == '+' or token == '-' or token == '*' or token == '/':
+                node = TreeNode(token)
+
+                if not stack.is_empty():
+                    right = stack.peek()
+                    stack.pop()
+                    node.right = right
+
+                    if not stack.is_empty():
