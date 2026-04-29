4b47991edaf97abe4f595f448273c8c43c6ec7de
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-04-29 16:58:27 -0400

    Finished the AddPatient method and added NextArrivalOrder to use with AddPatient. Also fixed typo: arrivel_counter instead of arrival_counter

--- M5 Trees and Priority Queues Project/TriageSystem.py
+++ M5 Trees and Priority Queues Project/TriageSystem.py
@@ -2,7 +2,7 @@ import heapq
 
 class TriageSystem:
 
-    arrivel_counter = 0
+    arrival_counter = 0
 
     def __init__(T):
         T.queue = []
@@ -12,3 +12,19 @@ class TriageSystem:
             raise ValueError("Patient must have a name.")
         if (severity < 1) or (severity > 5):
             raise ValueError("Severity must be between 1 and 5.")
+
+        arrival_order = TriageSystem.NextArrivalOrder()
+
+        heapq.heappush(T.queue, (-severity, arrival_order, name))
+
+
+
+
+
+
+    #Class-level static method for AddPatient
+    @staticmethod
+    def NextArrivalOrder():
+        current_arrival_order = TriageSystem.arrival_counter
+        TriageSystem.arrival_counter += 1
+        return current_arrival_order
