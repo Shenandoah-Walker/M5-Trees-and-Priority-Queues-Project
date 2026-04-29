1a5ca57821bd48112a1260e0b5d76976577d9a1b
Shenandoah_Walker <saw67402@email.vccs.edu>
2026-04-29 16:32:42 -0400

    Started writing code for TriageSystem Class

--- M5 Trees and Priority Queues Project/TriageSystem.py
+++ M5 Trees and Priority Queues Project/TriageSystem.py
@@ -1,1 +1,14 @@
+import heapq
+
 class TriageSystem:
+
+    arrivel_counter = 0
+
+    def __init__(T):
+        T.queue = []
+
+    def AddPatient(T, name, severity):
+        if name == None:
+            raise ValueError("Patient must have a name.")
+        if (severity < 1) or (severity > 5):
+            raise ValueError("Severity must be between 1 and 5.")
